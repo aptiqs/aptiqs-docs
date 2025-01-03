c23af93da10e2769c1437e82ab4f57a8af79f207
LarkBase <lakshman.kanadam@gmail.com>
2025-01-03 22:36:49 +0530

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+
+# Changelog
+
+## [1.0.0] - Initial Documentation
+- Added setup instructions.
+- Created contribution guidelines.
+- Launched documentation site.
---
+++ aptiqs-docs/docusaurus.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Aptiqs Documentation',
+  tagline: 'Your CLI Documentation Hub',
+  url: 'https://aptiqs.github.io/aptiqs-docs/',
+  baseUrl: '/aptiqs-docs/',
+  organizationName: 'aptiqs',
+  projectName: 'aptiqs-docs',
+  favicon: 'img/favicon.ico',
+  themeConfig: {
+    navbar: {
+      title: 'Aptiqs Docs',
+      items: [
+        { to: 'docs/', label: 'Docs', position: 'left' },
+        { href: 'https://github.com/aptiqs/aptiqs-docs', label: 'GitHub', position: 'right' },
+      ],
+    },
+  },
+};
